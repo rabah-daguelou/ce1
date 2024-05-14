aeddcb5929ef75d7be90e9a3bb1b6f9582edf220
lina <lin.dag@yahoo.com>
2024-05-14 20:51:23 +0200

    14/05/24

---
+++ travaux/travaux.js
@@ -0,0 +1,287 @@
+// Poésie
+function poesie() {
+  
+   // Texte d'introduction poésie Paul Eluard
+  let titreLiberte = document.createElement("h3");
+  titreLiberte.textContent = "Liberté ";
+  tableau.appendChild(titreLiberte);
+  let introduction = document.createElement("p");
+  introduction.innerHTML = `Dans le cadre d'un projet collectif en EMC 
+  (Education Morale et Civique), nous avons étudié les valeurs de
+  la République française. Sur le thème de la Liberté, nous avons choisis
+  d'étudier le poème <span> <a href="https://www.poetica.fr/poeme-279/liberte-paul-eluard/"
+  target='blanck'> Liberté </a> </span> , de Paul Eluard. Les élèves
+  ont pu écrire leur propre poème sur la même trame que l'auteur.
+  J'ai le plaisir de vous partager leurs productions.<br> <br>
+  <ul>
+  <li class='telecharger'> Cliquez sur la carte de l'élève pour la télécharger. </li>
+  <li class='audio'> Cliquez sur le lecteur audio pour écouter le poème de l'élève.</li>
+  <li class='composer'> En bas de la page, Cliquez sur "Composer mon poême" pour choisir aléatoirement des strophes de chaque élève. Cela doit les amuser.</li>
+  </ul>`;
+  tableau.append(introduction);
+
+  //  Liens vers la poésie
+  let divliens = document.createElement('div')
+  let divLienenfoires = document.createElement("div");
+  divLienenfoires.innerHTML= '<a href="https://www.youtube.com/watch?v=osk5BI-snio" target="blank"> Liberté chantée par les Enfoirés </a>'
+  let divLiendessinsAnimes = document.createElement("div");
+  divLiendessinsAnimes.innerHTML = '<a href="https://ladigitale.dev/digiview/#/v/640e8376c5681" target="blank"> Liberté en dessins animés </a>';
+  tableau.appendChild(divliens)
+  divliens.appendChild(divLienenfoires)
+  divliens.appendChild(divLiendessinsAnimes)
+
+  // Tableau des poésies élèves
+  let poesie = [
+    {
+      prenom: "Ninon",
+      pdf: "ninon.pdf",
+      png: "ninon.png",
+      audio: "ninon.mp3",
+      strophes: {
+        strophe1: "Ninon1.mp3",
+        strophe2: "Ninon2.mp3",
+        strophe3: "Ninon3.mp3",
+        strophe4: "Ninon4.mp3",
+      },
+    },
+    {
+      prenom: "Malek",
+      pdf: "malek.pdf",
+      png: "malek.png",
+      audio: "malek.mp3",
+      strophes: {
+        strophe1: "Malek1.mp3",
+        strophe2: "Malek2.mp3",
+        strophe3: "Malek3.mp3",
+        strophe4: "Malek4.mp3",
+      },
+    },
+    {
+      prenom: "Neil",
+      pdf: "neil.pdf",
+      png: "neil.png",
+      audio: "neil.mp3",
+      strophes: {
+        strophe1: "Neil1.mp3",
+        strophe2: "Neil2.mp3",
+        strophe3: "Neil3.mp3",
+        strophe4: "Neil4.mp3",
+      },
+    },
+    {
+      prenom: "Abdou-Rahmen",
+      pdf: "abdou-rahmen.pdf",
+      png: "abdou-rahmen.png",
+      audio: "abdou.mp3",
+      strophes: {
+        strophe1: "Abdou1.mp3",
+        strophe2: "Abdou2.mp3",
+        strophe3: "Abdou3.mp3",
+        strophe4: "Abdou4.mp3",
+      },
+    },
+    {
+      prenom: "Anais_N",
+      pdf: "anais_n.pdf",
+      png: "anais_n.png",
+      audio: "anais_n.mp3",
+      strophes: {
+        strophe1: "Anais_n1.mp3",
+        strophe2: "Anais_n2.mp3",
+        strophe3: "Anais_n3.mp3",
+        strophe4: "Anais_n4.mp3",
+      },
+    },
+    {
+      prenom: "Inès",
+      pdf: "ines.pdf",
+      png: "ines.png",
+      audio: "ines.mp3",
+      strophes: {
+        strophe1: "Ines1.mp3",
+        strophe2: "Ines2.mp3",
+        strophe3: "Ines3.mp3",
+        strophe4: "Ines4.mp3",
+      },
+    },
+    {
+      prenom: "Iyed",
+      pdf: "iyed.pdf",
+      png: "iyed.png",
+      audio: "iyed.mp3",
+      strophes: {
+        strophe1: "Iyed1.mp3",
+        strophe2: "Iyed2.mp3",
+        strophe3: "Iyed3.mp3",
+        strophe4: "Iyed4.mp3",
+      },
+    },
+    {
+      prenom: "Jannat",
+      pdf: "jannat.pdf",
+      png: "jannat.png",
+      audio: "jannat.mp3",
+      strophes: {
+        strophe1: "Jannat1.mp3",
+        strophe2: "Jannat2.mp3",
+        strophe3: "Jannat3.mp3",
+        strophe4: "Jannat4.mp3",
+      },
+    },
+    {
+      prenom: "Mayssa",
+      pdf: "mayssa.pdf",
+      png: "mayssa.png",
+      audio: "mayssa.mp3",
+      strophes: {
+        strophe1: "Mayssa1.mp3",
+        strophe2: "Mayssa2.mp3",
+        strophe3: "Mayssa3.mp3",
+        strophe4: "Mayssa4.mp3",
+      },
+    },
+    {
+      prenom: "Nour-Aicha",
+      pdf: "nour-aicha.pdf",
+      png: "nour-aicha.png",
+      audio: "nour-aicha.mp3",
+      strophes: {
+        strophe1: "Nour-aicha1.mp3",
+        strophe2: "Nour-aicha2.mp3",
+        strophe3: "Nour-aicha3.mp3",
+        strophe4: "Nour-aicha4.mp3",
+      },
+    },
+    {
+      prenom: "Sofija",
+      pdf: "sofija.pdf",
+      png: "sofija.png",
+      audio: "sofija.mp3",
+      strophes: {
+        strophe1: "Sofija1.mp3",
+        strophe2: "Sofija2.mp3",
+        strophe3: "Sofija3.mp3",
+        strophe4: "Sofija4.mp3",
+      },
+    },
+    {
+      prenom: "Yasmine",
+      pdf: "yasmine.pdf",
+      png: "yasmine.png",
+      audio: "yasmine.mp3",
+      strophes: {
+        strophe1: "Yasmine1.mp3",
+        strophe2: "Yasmine2.mp3",
+        strophe3: "Yasmine3.mp3",
+        strophe4: "Yasmine4.mp3",
+      },
+    },
+    {
+      prenom: "Youssef",
+      pdf: "youssef.pdf",
+      png: "youssef.png",
+      audio: "youssef.mp3",
+      strophes: {
+        strophe1: "Youssef1.mp3",
+        strophe2: "Youssef2.mp3",
+        strophe3: "Youssef3.mp3",
+        strophe4: "Youssef4.mp3",
+      },
+    },
+    {
+      prenom: "Djibril",
+      pdf: "djibril.pdf",
+      png: "djibril.png",
+      audio: "djibril.mp3",
+      strophes: {
+        strophe1: "Djibril1.mp3",
+        strophe2: "Djibril2.mp3",
+        strophe3: "Djibril3.mp3",
+        strophe4: "Djibril4.mp3",
+      },
+    },
+    {
+      prenom: "Aéron",
+      pdf: "aeron.pdf",
+      png: "aeron.png",
+      audio: "aeron.mp3",
+      strophes: {
+        strophe1: "Aeron1.mp3",
+        strophe2: "Aeron2.mp3",
+        strophe3: "Aeron3.mp3",
+        strophe4: "Aeron4.mp3",
+      },
+    },
+    {
+      prenom: "Anaîs_A",
+      pdf: "anais_a.pdf",
+      png: "anais_a.png",
+      audio: "anais_a.mp3",
+      strophes: {
+        strophe1: "Anais_a1.mp3",
+        strophe2: "Anais_a2.mp3",
+        strophe3: "Anais_a3.mp3",
+        strophe4: "Anais_a4.mp3",
+      },
+    },
+    {
+      prenom: "Adam",
+      pdf: "adam.pdf",
+      png: "adam.png",
+      audio: "adam.mp3",
+      strophes: {
+        strophe1: "Adam1.mp3",
+        strophe2: "Adam2.mp3",
+        strophe3: "Adam3.mp3",
+        strophe4: "Adam4.mp3",
+      },
+    },
+  ];
+
+  // Créer la div containeur des poèmes
+  let divPoemes = document.createElement("div");
+  divPoemes.className = "divPoemes";
+
+  tableau.appendChild(divPoemes);
+
+  // Parcourir le tableau des poèmes et créer la liste
+  poesie.forEach((eleve) => {
+    let divPoeme = document.createElement("div");
+    let prenomPoeme = document.createElement("h5");
+    let imagePoeme = document.createElement("div");
+    let divAudio = document.createElement("div");
+
+    divPoeme.className = "divPoeme";
+    imagePoeme.className = "imagePoeme";
+    prenomPoeme.className = "prenomPoeme";
+    divAudio.className = "divAudio";
+
+    divPoemes.appendChild(divPoeme);
+    divPoeme.appendChild(prenomPoeme);
+    divPoeme.appendChild(imagePoeme);
+    divPoeme.appendChild(divAudio);
+
+    prenomPoeme.textContent = eleve.prenom;
+    imagePoeme.innerHTML = `<a href="poesie/poemesPdf/${eleve.pdf}" target="_blank"><img src="poesie/poemesPng/${eleve.png}"  alt="Poésie de ${ eleve.prenom }"></a>`;
+    divAudio.innerHTML = `<audio controls src="poesie/audios/${eleve.audio}" ></audio>`;
+  });
+
+}
+/*
+// Créer mon poème
+  let btnPoemeAleatoire = document.createElement('button');
+  btnPoemeAleatoire.className = 'btn btn-success';
+  tableau.appendChild(btnPoemeAleatoire);
+  btnPoemeAleatoire.textContent = 'Générer un poème';
+  let poemeGenere= document.createElement('audio')
+
+  // Ecouter le bouton
+  btnPoemeAleatoire.addEventListener('click', ()=> {
+    
+    let nombreAleatoire = aleatoire(1, 5)
+  })
+
+
+
+function aleatoire(min, max) { return Math.trunc(min + Math.random() * (max - min)) };
+*/
