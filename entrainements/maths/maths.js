78660e6bb2e1a4d0a7c8c0e3c9d9c2ad7bedcbcb
lina <lin.dag@yahoo.com>
2024-05-20 17:01:43 +0200

    20/05/24

--- entrainements/maths/maths.js
+++ entrainements/maths/maths.js
@@ -8,39 +8,116 @@ let tableau = document.getElementById("tableau");
 let bonneRéponse = "Bravo! Bonne réponse.";
 let mauvaiseRéponse = "Désolé! Mauvaise réponse.";
 let messageReponse = document.createElement("p");
-messageReponse.className = "messageReponse";
+    messageReponse.className = "messageReponse";
 
 // Consignes
 let consigne = document.createElement("p");
 consigne.className = "consigne";
 let consigneCalculMental = `<h2> Consigne </h2> <p> Tu dois répondre à 10 questions. Chaque bonne réponse te rapportera 1 point. Tu peux voir ta note qui s'affichera en haut à droite du tableau. </p>`;
+let consigneAdditionPosee = `<h2> Consigne </h2> <p> Tu dois faire 10 additions posées. Chaque bonne réponse te rapportera 1 point. Tu peux voir ta note qui s'affichera en haut à droite du tableau. </p>`;
+let consigneSoustractionPosee = `<h2> Consigne </h2> <p> Tu dois faire 10 soustractions posées. Chaque bonne réponse te rapportera 1 point. Tu peux voir ta note qui s'affichera en haut à droite du tableau. </p>`;
+let consigneMultiplicationPosee = `<h2> Consigne </h2> <p> Tu dois faire 10 Multiplications posées. Chaque bonne réponse te rapportera 1 point. Tu peux voir ta note qui s'affichera en haut à droite du tableau. </p>`;
+let consigneTablesDeMultiplication = `<h2> Consigne </h2> <p> Tu dois choisir une table de multiplication. Chaque bonne réponse te rapportera 1 point. Tu peux voir ta note qui s'affichera en haut à droite du tableau. </p>`;
+
+
 
 // Bouton commencer
 let bouton_commencer = document.createElement("button");
 bouton_commencer.textContent = "Commencer";
 bouton_commencer.className = "btn btn-danger bouton_commencer";
-// Fonction niveau
-function niveauFunction(niveau) {
-  switch (niveau) {
-    case 1:
-      n1 = Math.trunc(Math.random() * 10);
-      n2 = Math.trunc(Math.random() * 10);
+
+// Clique sur un choix
+function clique(choix, niveau, note, question) {
+  tableau.innerHTML = "";
+  console.log("Choix:", choix, "Niveau:", niveau);
+  tableau.appendChild(consigne);
+  tableau.appendChild(bouton_commencer);
+
+  switch (choix) {
+    // 1. Calcul mental
+    case "Calcul mental":
+      consigne.innerHTML = consigneCalculMental;
+      bouton_commencer.addEventListener("click", () => {
+        calculFunction(choix, niveau, note, question);
+      });
       break;
-    case 2:
-      n1 = Math.trunc(Math.random() * 20);
-      n2 = Math.trunc(Math.random() * 20);
+    // 2. Addition posée
+    case "Addition posée":
+      consigne.innerHTML = consigneAdditionPosee;
+      bouton_commencer.addEventListener("click", () => {
+        calculFunction(choix, niveau, note, question);
+      });
       break;
-    case 3:
-      n1 = Math.trunc(Math.random() * 100);
-      n2 = Math.trunc(Math.random() * 100);
+    // 3. Soustraction posée
+    case "Soustraction posée":
+      consigne.innerHTML = consigneSoustractionPosee;
+      bouton_commencer.addEventListener("click", () => {
+        calculFunction(choix, niveau, note, question);
+      });
+      break;
+    // 4. Multiplication posée
+    case "Multiplication posée":
+      consigne.innerHTML = consigneMultiplicationPosee;
+      bouton_commencer.addEventListener("click", () => {
+        calculFunction(choix, niveau, note, question);
+      });
+      break;
+    // 5. Tables de multiplication
+    case "Tables de multiplication":
+      consigne.innerHTML = consigneTablesDeMultiplication;
+      bouton_commencer.addEventListener("click", () => {
+        tablesDeMultiplicationFunction(choix, niveau, note, question);
+      });
       break;
     default:
       break;
   }
 }
 
-// Fonction opération
-function operationFunction(numero_operation, niveau) {
+// Operation Function
+function operationFunction(choix, niveau, note, question) {
+  
+  switch (choix) {
+    case "Calcul mental":
+      calculMentalOperationFunction(niveau);
+      break;
+    case "Addition posée":
+      additionPoseeOperationFunction(niveau);
+      break;
+    case "Soustraction posée":
+      soustractionPoseeOperationFunction(niveau);
+      break;
+    case "Multiplication posée":
+      multiplicationPoseeOperationFunction(niveau);
+      break;
+    
+    default:
+      break;
+  }
+}
+
+//1. Opération calcul mental // calculMentalOperationFunction
+function calculMentalOperationFunction(niveau) {
+      
+  switch (niveau) {
+      
+    case 1:
+      n1 = Math.trunc(Math.random(100, 1000) * 10);
+      n2 = Math.trunc(Math.random(100, 1000) * 10);
+      break;
+    case 2:
+      n1 = Math.trunc(Math.random(100, 1000) * 20);
+      n2 = Math.trunc(Math.random(100, 1000) * 20);
+      break;
+    case 3:
+      n1 = Math.trunc(Math.random(100, 1000) * 100);
+      n2 = Math.trunc(Math.random(100, 1000) * 100);
+      break;
+    default:
+      break;
+  }
+  let numero_operation = Math.floor(Math.random() * 3);
+
   switch (numero_operation) {
     // Addition
     case 0:
@@ -48,6 +125,7 @@ function operationFunction(numero_operation, niveau) {
       nom_operation = " Addition ";
       operation = `${n1} + ${n2} `;
       resultat = n1 + n2;
+      console.log ("Calcul mental addition niveau:", niveau)
       break;
     // Soustraction
     case 1:
@@ -60,6 +138,7 @@ function operationFunction(numero_operation, niveau) {
         operation = `${n2} - ${n1} `;
         resultat = n2 - n1;
       }
+      console.log("Calcul mental soustraction niveau:", niveau);
       break;
     // Multiplication
     case 2:
@@ -76,46 +155,375 @@ function operationFunction(numero_operation, niveau) {
         operation = `${n1} X ${n2} `;
         resultat = n2 * n1;
       }
+      console.log("Calcul mental multiplication niveau:", niveau);
       break;
     default:
       break;
   }
 }
 
-// Clique sur Calcul mental
-function clique(choix, niveau, note, question) {
-  console.log('Choix:', choix);
-  console.log('Niveau:', niveau);
+//2-  Opération addition posée // additionPoseeOperationFunction
+function additionPoseeOperationFunction(niveau) {
+  let consigneAdditionPosee2 = document.createElement('p');
+  consigneAdditionPosee2.textContent=' Pose ton opération sur ton cahier ou ton ardoise, introduit ton résultat puis clique sur "Valider"'
+  tableau.appendChild(consigneAdditionPosee2)
+    
+  switch (niveau) {
+    case 1:
+      n1 = Math.trunc(Math.random(100, 1000) * 100);
+      n2 = Math.trunc(Math.random(100, 1000) * 100);
+      operation = `${n1} + ${n2} `;
+      resultat = n1+ n2;
+      break;
+    case 2:
+      n1 = Math.trunc(Math.random(100, 1000) * 200);
+      n2 = Math.trunc(Math.random(100, 1000) * 200);
+      operation = `${n1} + ${n2} `;
+      resultat = n1+ n2;
+        
+      break;
+    case 3:
+        n1 = Math.trunc(Math.random(100, 1000) * 1000);
+        n2 = Math.trunc(Math.random(100, 1000) * 1000);
+        operation = `${n1} + ${n2} `;
+        resultat = n1+ n2;
+      break;
+    default:
+      break;
+  }
+}
+
+//3-  Opération Soustraction posée // soustractionPoseeOperationFunction
+function soustractionPoseeOperationFunction(niveau) {
+  let consigneSoustractionPosee2 = document.createElement('p');
+  consigneSoustractionPosee2.textContent=' Pose ton opération sur ton cahier ou ton ardoise, introduit ton résultat puis clique sur "Valider"'
+  tableau.appendChild(consigneSoustractionPosee2)
+    
+  switch (niveau) {
+    case 1:
+      n1 = Math.trunc(Math.random(100, 1000) * 100);
+      n2 = Math.trunc(Math.random(100, 1000) * 100);
+      if (n1 > n2) {
+        operation = `${n1} - ${n2} `;
+        resultat = n1 - n2;
+      } else {
+        operation = `${n2} - ${n1} `;
+        resultat = n2 - n1;
+      }
+      
+      break;
+    case 2:
+      n1 = Math.trunc(Math.random(100, 1000) * 200);
+      n2 = Math.trunc(Math.random(100, 1000) * 200);
+      if (n1 > n2) {
+        operation = `${n1} - ${n2} `;
+        resultat = n1 - n2;
+      } else {
+        operation = `${n2} - ${n1} `;
+        resultat = n2 - n1;
+      }
+        
+      break;
+    case 3:
+        n1 = Math.trunc(Math.random(100, 1000) * 1000);
+        n2 = Math.trunc(Math.random(100, 1000) * 1000);
+        if (n1 > n2) {
+          operation = `${n1} - ${n2} `;
+          resultat = n1 - n2;
+        } else {
+          operation = `${n2} - ${n1} `;
+          resultat = n2 - n1;
+        }
+      break;
+    default:
+      break;
+  }
+}
+
+// 4. multiplicationPoseeOperationFunction(niveau)
+function multiplicationPoseeOperationFunction(niveau) {
+  let consigneMultiplicationPosee2 = document.createElement("p");
+  consigneMultiplicationPosee2.textContent =
+    ' Pose ton opération sur ton cahier ou ton ardoise, introduit ton résultat puis clique sur "Valider"';
+  tableau.appendChild(consigneMultiplicationPosee2);
+
+  switch (niveau) {
+    case 1:
+      n1 = Math.trunc(Math.random(100, 1000) * 100);
+      n2 = Math.trunc(Math.random(100, 1000) * 10);
+      operation = `${n1} X ${n2} `;
+      resultat = n1 * n2;
+      break;
+    case 2:
+      n1 = Math.trunc(Math.random(100, 1000) * 200);
+      n2 = Math.trunc(Math.random(100, 1000) * 20);
+      operation = `${n1} X ${n2} `;
+      resultat = n1 * n2;
+
+      break;
+    case 3:
+      n1 = Math.trunc(Math.random(100, 1000) * 1000);
+      n2 = Math.trunc(Math.random(100, 1000) * 1000);
+      operation = `${n1} X ${n2} `;
+      resultat = n1 * n2;
+      break;
+    default:
+      break;
+  }
+}
+
+// 5. Tables de multiplication(niveau)
+function tablesDeMultiplicationFunction(choix, niveau, note, question) {
   tableau.innerHTML = "";
-  switch (choix) {
-    case 'calcul_mental':
-      tableau.appendChild(consigne);
-      consigne.innerHTML = consigneCalculMental;
-      tableau.appendChild(bouton_commencer);
-      bouton_commencer.addEventListener("click", () => {
-      calcul_mental_function(niveau, note, question);
-      });
+
+  consigne.textContent =
+    ' Choisie ta table de multiplication, introduit ton résultat puis clique sur "Valider".';
+  tableau.appendChild(consigne);
+  let tables = [2, 5, 10, 3, 4, 6, 7, 8, 9];
+  let divBoutonsTables = document.createElement("div");
+  divBoutonsTables.className = "divBoutonsTables";
+  tableau.appendChild(divBoutonsTables);
+  let table
+  // Choix de la table
+  switch (niveau) {
+    case 1:
+      for (let t = 0; t < 3; t++) {
+        let monBoutonTable = document.createElement("button");
+        monBoutonTable.className = `btn${t} btn btn-success`;
+        monBoutonTable.textContent = `Table de ${tables[t]}`;
+        divBoutonsTables.appendChild(monBoutonTable);
+        monBoutonTable.addEventListener('click', () => {
+          table = tables[t]
+          tableFunction(table, note, question)
+        }) 
+      }
       break;
-  
+    case 2:
+      for (let t = 3; t < 6; t++) {
+        let monBoutonTable = document.createElement("button");
+        monBoutonTable.className = `btn${t} btn btn-success`;
+        monBoutonTable.textContent = `Table de ${tables[t]}`;
+        divBoutonsTables.appendChild(monBoutonTable);
+        monBoutonTable.addEventListener('click', () => {
+          table = tables[t]
+          tableFunction(table, note, question)
+        })
+      }
+      break;
+    case 3:
+      for (let t = 6; t < 9; t++) {
+        let monBoutonTable = document.createElement("button");
+        monBoutonTable.className = `btn${t} btn btn-success`;
+        monBoutonTable.textContent = `Table de ${tables[t]}`;
+        divBoutonsTables.appendChild(monBoutonTable);
+        monBoutonTable.addEventListener('click', () => {
+          table = tables[t]
+          tableFunction(table, note, question)
+        })
+      }
+      break;
     default:
       break;
   }
-  
-  
-  
-  
-  
 
-  // Bouton Commencer
-  
   
 }
+// Fonction tables multiplication
+function tableFunction(x, note, question) {
+  tableau.innerHTML = ''
+  // Créer h2 Titre
+  let titreAndScore = document.createElement("div");
+  titreAndScore.className = "titreAndScore";
+  let titre = document.createElement("div");
+  titre.className = "titre";
+  let titre1 = document.createElement("h4");
+  titre1.textContent = 'Table de: ' + x;
+  let titre2 = document.createElement("h4");
+  titre2.textContent = "Niveau: CP / CE1 ";
+  // Créer p Score
+  let score = document.createElement("h5");
+  
+  // Afficher le titre et le score
+  tableau.appendChild(titreAndScore);
+  titreAndScore.appendChild(titre);
+  titre.appendChild(titre1);
+  tableau.appendChild(titre2);
+  titreAndScore.appendChild(score);
+  score.textContent = `Note : ${note} / 10 `;
+  
+  // Afficher la table
+  let divFormulaire = document.createElement('div')
+  divFormulaire.className = 'divFormulaire'
+  let formulaire = document.createElement("form");
+  formulaire.className='formulaire'
+  divFormulaire.appendChild(formulaire)
 
-// Fonction Calcul mental
-function calcul_mental_function(niveau, note, question) {
-  //
+  tableau.appendChild(divFormulaire)
 
-  //
+  for (let i = 1; i < 11; i++) {
+    let divLabelInput = document.createElement('div')
+    divLabelInput.className='divLabelInput'
+    formulaire.appendChild(divLabelInput)
+    
+    let myLabel = document.createElement('label')
+    let myInput = document.createElement('input')
+    myInput.required = true
+    
+    myLabel.textContent = `${x} x ${i} = `
+    divLabelInput.appendChild(myLabel);
+    divLabelInput.appendChild(myInput)
+  }
+  
+  // Créer le bouton valider
+  let divBoutonValider = document.createElement("div");
+  let boutonValider = document.createElement('button')
+  boutonValider.textContent = 'Valider'
+  boutonValider.className = 'boutonValider btn btn-success'
+  
+  formulaire.appendChild(divBoutonValider)
+  divBoutonValider.appendChild(boutonValider)
+  
+  let tableauInputs = document.getElementsByTagName('input');
+  
+  //  Vérifier les inputs
+  formulaire.addEventListener('submit', function (f) {
+    note = 0;
+    for (let j = 1; j < tableauInputs.length+1; j++) {
+      if (tableauInputs[j-1].value) {
+        tableauInputs[j-1].disabled = true
+      }
+      console.log(x,j);
+        if (tableauInputs[j-1].value == x * j) {
+          tableauInputs[j - 1].style.background = 'green'
+          tableauInputs[j - 1].style.color = "white";
+      note++
+      score.textContent = `Note : ${note} / 10`;
+    } else {
+          tableauInputs[j - 1].style.background = "red";
+          tableauInputs[j - 1].style.color = "white";
+      }
+    }
+    f.preventDefault();
+  })
+}
+
+// Fonction générale calculFunction
+
+function calculFunction(choix, niveau, note, question) {
+  console.log(`Choix: ${choix} Niveau: ${niveau}`);
+  tableau.innerHTML = "";
+  let titreAndScore = document.createElement("div");
+  titreAndScore.className = "titreAndScore";
+
+  // Créer h2 Titre
+  let titre = document.createElement("div");
+  titre.className = "titre";
+  let titre1 = document.createElement("h4");
+  titre1.textContent = choix;
+  let titre2 = document.createElement("h4");
+  titre2.textContent = "Niveau:  " + niveau;
+  // Créer p Score
+  let score = document.createElement("p");
+  score.textContent = `Note : ${note} / ${question} `;
+
+  // Afficher le titre et le score
+  tableau.appendChild(titreAndScore);
+
+  titreAndScore.appendChild(titre);
+  titre.appendChild(titre1);
+  titre.appendChild(titre2);
+  titreAndScore.appendChild(score);
+
+  // Créer l'opération
+  operationFunction(choix, niveau);
+
+  let texteOperation = document.createElement("p");
+  texteOperation.className = "operation";
+  texteOperation.textContent = operation;
+  tableau.appendChild(texteOperation);
+
+  // Créer input réponse
+  let inputReponse = document.createElement("input");
+  inputReponse.className = "btn btn-light";
+  inputReponse.setAttribute("placeholder", "Ta réponse ici");
+  tableau.appendChild(inputReponse);
+
+  // Apparaitre le bouton Valider
+  let bouton_valider = document.createElement("button");
+  bouton_valider.textContent = "Valider";
+  bouton_valider.className = "btn btn-danger bouton_valider";
+
+  // Ecouter input insérer bouton_valider
+  inputReponse.addEventListener("input", () =>
+    tableau.appendChild(bouton_valider)
+  );
+  console.log("Réponse:", inputReponse.value);
+  // Ecouter bouton_valider - Insérer le bouton Suivant
+  bouton_valider.addEventListener("click", () => {
+    bouton_valider.remove();
+    inputReponse.toggleAttribute("disabled");
+    // Créer insérer bouton Suivant
+    let bouton_suivant = document.createElement("button");
+    bouton_suivant.textContent = "Suivant";
+    bouton_suivant.className = "btn btn-success bouton_suivant";
+
+    tableau.appendChild(messageReponse);
+    tableau.appendChild(bouton_suivant);
+    question++;
+
+    // Tester la réponse
+    if (inputReponse.value == resultat) {
+      note++;
+      score.textContent = `Note : ${note} / ${question} `;
+      messageReponse.style.color = "green";
+      messageReponse.textContent = bonneRéponse;
+    } else {
+      messageReponse.style.color = "red";
+      messageReponse.textContent = mauvaiseRéponse;
+    }
+
+    // Ecouter le bouton suivant
+    bouton_suivant.addEventListener("click", () => {
+      tableau.innerHTML = "";
+      if (question < 10) {
+        calculFunction(choix, niveau, note, question);
+      } else {
+        let noteFinale = document.createElement("p");
+        tableau.appendChild(noteFinale);
+        noteFinale.className = "noteFinale";
+        noteFinale.textContent = `Ta note est:  ${note} / 10 `;
+
+        let commentaire = document.createElement("p");
+        tableau.appendChild(commentaire);
+        commentaire.className = "commentaire";
+
+        let emoji = document.createElement("p");
+        tableau.appendChild(emoji);
+        emoji.className = "emoji";
+
+        if (note < 5) {
+          commentaire.textContent =
+            "Résultats insufisants! Tu dois t'entrainer davantage.";
+          emoji.innerHTML = "&#128532;";
+        } else if (note < 9) {
+          commentaire.textContent =
+            "Résultats satisfaisants! Tu peux mieux faire.";
+          emoji.innerHTML = "&#128512;";
+        } else {
+          commentaire.textContent =
+            "Excellents résultats! Tu peux essayer le niveau supérieur.";
+          emoji.innerHTML = "&#128585;";
+        }
+      }
+    });
+  });
+};
+
+  
+/*
+// 1. Fonction Calcul mental
+function calcul_mental_function(choix, niveau, note, question) {
+ 
   tableau.innerHTML = "";
   let numero_operation = Math.floor(Math.random() * 3);
 
@@ -128,8 +536,8 @@ function calcul_mental_function(niveau, note, question) {
   titreAndScore.className = "titreAndScore";
 
   // Créer h2 Titre
-  let titre = document.createElement("h2");
-  titre.textContent = " Calcul mental";
+  let titre = document.createElement("h4");
+  titre.textContent = choix + niveau;
 
   // Créer p Score
   let score = document.createElement("p");
@@ -222,7 +630,5 @@ function calcul_mental_function(niveau, note, question) {
     });
   });
 }
-
-// Clique sur Addition posée
-
-// Fonction Addition posée
+*/
+// 2. Fonction Addition posée
