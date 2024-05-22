99efb4a4ad876bb66a63979b0bb82887ccad69b3
lina <lin.dag@yahoo.com>
2024-05-22 13:58:47 +0200

    22/05/24

--- devoirs/devoirs.js
+++ devoirs/devoirs.js
@@ -1,35 +1,5 @@
 let clique = document.getElementById('clique');
 let entrainementDiv = document.getElementById('entrainement')
 
-// Cliquer sur entrainement
-clique.addEventListener('click', () => {
-    entrainer()
-})
 
-// Message
-
-let messageP = document.getElementById('message')
-
-// Fonction du clique entrainement
-function entrainer() {
-    
-    entrainementDiv.classList.toggle("entrainement")
-    if (entrainementDiv.className = 'entrainement') {
-        clique.textContent = 'Recommencer '
-        messageP.textContent = "Pose ton opération puis clique sur SOLUTION pour te corriger."
-        let number1 = Math.trunc(Math.random(100, 1000) * 10000)
-        let number2 = Math.trunc(Math.random(100, 1000) * 10000);
-        entrainementDiv.innerHTML =
-          number1 +
-          " + " +
-          number2 +
-          " = " +
-          '<span> <button type="button" class="btn btn-dark solution">Solution</button></span>';
-        let solution = document.querySelector(".solution");
-        solution.addEventListener('click', () => {
-        solution.textContent = number1 + number2
-        })
-    
-}
-}
 
