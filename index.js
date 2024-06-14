a10e9faff71e06cbb72ad2180129f07763b28626
lina <lin.dag@yahoo.com>
2024-06-14 19:41:41 +0200

    14/06/2024

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ btnHumberger.addEventListener("click", () => {
   btnHumberger.classList.toggle("active");
   navbarNav.classList.toggle("menuBackground");
 });
+
+// Popup
+
+let popup = document.getElementById('popup')
+let popupBtn = document.getElementById("popupBtn");
+
+popupBtn.addEventListener('click', () => {
+  popup.style.display='none'
+})
